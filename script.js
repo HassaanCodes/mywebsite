737fc2fc202a16535a5dfcc89cc92381863627e4
HassaanCodes <hassaanasif508@gmail.com>
2026-04-12 16:57:50 -0300

    Updated Blogs page

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    console.log("Page Loaded");
 
-    document.documentElement.style.scrollBehavior = "smooth";
+console.log("Your script is running")
 
-    document.querySelectorAll(".project-images").forEach(project => {
-        project.addEventListener('click', () => {
-            window.open(project.dataset.link, '_blank', 'noopener')
-        })
+const params = new URLSearchParams(window.location.search)
+const post = params.get('post')
+console.log("post:", post);
+
+if (post) {
+
+
+    console.log("fetching:", `posts/${post}.json`);
+
+    fetch(`../posts/${post}.json`)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("response not found")
+        }
+        
+        return res.json()
+
     })
-})
+    .then(data => {
+        document.querySelector(".jacob").innerHTML = `
+        <section class = 'article'>
+            <h1>${data.title}</h1>
+            <p>${data.content}</p>
+        </section>
+        `
+    })
+    .catch(err => {
+        document.querySelector('.jacob').innerHTML = `
+            <h1>404</h1>
+            <p>post not found</p>
+        `
+    })
+}
